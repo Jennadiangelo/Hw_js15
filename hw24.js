49042097a5b9159fa916e1e7ce3ef819d0d2d919
yevheniiapetruk <yevheniiapetruk@gmail.com>
2023-10-04 17:18:22 +0200

    new

--- hw24.js
+++ hw24.js
@@ -2,7 +2,7 @@
 Create a function printNumber
 printNumber should print numbers from 1 to 5
 Print each number every second
-Try using setTimeout and setInterval*//*
+Try using setTimeout and setInterval*/
 const printNumber = () => {
     let num = 1;
     const sec = setInterval(print, 1000);
@@ -13,12 +13,12 @@ const printNumber = () => {
       } 
     }
 }
-printNumber();*/
+printNumber();
 
 /*Delayed loop
 Loop though from 1 to 5
 Print number with a 1s delay
-*//*
+*/
 const delayedLoop = () => {
   for (let i = 1; i <= 5; i++) {
     setTimeout(() => {
@@ -27,16 +27,29 @@ const delayedLoop = () => {
   }
 };
 
-delayedLoop();*/
+delayedLoop();
 
 /*Random
 Create a function randomPromise
 randomPromise should return promise
 randomPromise should resolve promise if random number >= 0.5
 randomPromise should reject promise if random number <> 0.5
+*/
+const randomPromise = () => {
+  return new Promise((resolve, reject) => {
+    const randnum = Math.random(); 
+    if (randnum >= 0.5) {
+      resolve(`${randnum} is >= 0.5`);
+    } else {
+      reject(`${randnum} is < 0.5`);
+    }
+  });
+}
+randomPromise()
+  .then((randnum) => {
+    console.log(randnum);
+  })
 
-const getUserData = (id) => {
-  // your code
-};
-// code for error
-// throw Error("404 not found");*/
+  .catch((randnum) => {
+    console.log(randnum);
+  });
